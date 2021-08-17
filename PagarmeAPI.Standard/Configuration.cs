9865d098bb90c5850781db8768cf71a6568aada5
Thiago Barradas <th.barradas@gmail.com>
2021-08-17 12:44:01 +0000

    pix_recipient_as_source de recipient agora é anulavel.

---
+++ PagarmeAPI.Standard/Configuration.cs
@@ -0,0 +1,15 @@
+using PagarmeAPI.Standard.Models;
+namespace PagarmeAPI.Standard
+{
+    public partial class Configuration
+    {
+
+
+        //The base Uri for API calls
+        public static string BaseUri = "https://api.pagar.me/1";
+
+        //TODO: Replace the ApiKey with an appropriate value
+        public static string ApiKey = "";
+
+    }
+}
