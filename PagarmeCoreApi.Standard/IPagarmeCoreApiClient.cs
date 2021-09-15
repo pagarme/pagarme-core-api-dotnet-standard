1c76ad247c6de0fe3af913ccef0196c533dc902a
Magno <magnomoreira@yahoo.com>
2021-09-15 14:44:59 -0300

    5.0.1 Include Accepted Brands (#6)
    Co-authored-by: Thiago Barradas <tbarradas@mundipagg.com>

--- PagarmeCoreApi.Standard/IPagarmeCoreApiClient.cs
+++ PagarmeCoreApi.Standard/IPagarmeCoreApiClient.cs
@@ -22,24 +22,19 @@ namespace PagarmeCoreApi.Standard
         ISubscriptionsController Subscriptions { get;}
 
         /// <summary>
-        /// Singleton access to Invoices controller
-        /// </summary>
-        IInvoicesController Invoices { get;}
-
-        /// <summary>
         /// Singleton access to Orders controller
         /// </summary>
         IOrdersController Orders { get;}
 
         /// <summary>
+        /// Singleton access to Invoices controller
+        /// </summary>
+        IInvoicesController Invoices { get;}
+
+        /// <summary>
         /// Singleton access to Customers controller
         /// </summary>
         ICustomersController Customers { get;}
-
-        /// <summary>
-        /// Singleton access to Recipients controller
-        /// </summary>
-        IRecipientsController Recipients { get;}
 
         /// <summary>
         /// Singleton access to Charges controller
@@ -50,6 +45,11 @@ namespace PagarmeCoreApi.Standard
         /// Singleton access to Transfers controller
         /// </summary>
         ITransfersController Transfers { get;}
+
+        /// <summary>
+        /// Singleton access to Recipients controller
+        /// </summary>
+        IRecipientsController Recipients { get;}
 
         /// <summary>
         /// Singleton access to Tokens controller
